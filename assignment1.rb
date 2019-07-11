c3bb13aedb71252de873e02e1e1e4de5991de2b0
alphabin <ec2-user@ip-172-31-32-181.us-east-2.compute.internal>
2019-07-11 07:20:25 +0000

    updates with Rspec passed

--- assignment1.rb
+++ assignment1.rb
@@ -1,3 +1,5 @@
+#Chatura Ahangama CSUMB 7/11/19
+
 class CountLetters 
 
   # define an instance variable containing empty hash with key being the letter
@@ -15,8 +17,10 @@ class CountLetters
 
   # given a filename, read the file and for each letter update the hash table  
   def analyzeFile(file) 
-  
-   
+    IO.foreach(file) {|x| 
+      #replaces \r\n
+      x = x.gsub(/\r\n+/, "")
+      @counts[x] += 1 }
   end  
   
   # return an array of lists  
@@ -24,21 +28,43 @@ class CountLetters
   #  Only letters with count >0 are returned.
   #  Array is in decreasing order by count.
   def sortByCountDecreasing
-    
+      sortedDec = @counts.sort{|a,b| b[1]<=>a[1]}
+      sortedDec.to_a
   end
   
   # return an array  [  string, count ]
   #  string is the letter or letters that occur the most
   #  count of occurrences
   def highestfrequency
-     
+     gatherKeys = ""
+     valueHigh = 0
+     @counts.each { |k, v| 
+        if v ==  @counts.values.max
+           gatherKeys += k 
+           valueHigh = v 
+        end
+      }
+     returnval = { gatherKeys => valueHigh}.to_a[0]
+     #doing a last minute check
+     if(gatherKeys == "")
+        returnval = nil
+     end
+     returnval
   end
   
   # return array of length 11. Each element  is [letter, count].  
   # [ ["A", count], ["B", count], ... , ["K", count] ] 
   # array is in order by letters
   def sortByLetter
-     
+     hashStore = {}
+     ("A".."K").each{ |letter|
+      if @counts.key?(letter)
+          hashStore[letter] = @counts[letter]
+      else
+          hashStore[letter] = 0
+      end
+     }
+     hashStore.to_a
   end
   
 end
